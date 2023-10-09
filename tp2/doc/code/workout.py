514c6d545f8cfea85bd9c4dd5bd0cb32f1912a7a
sh4rk <lucasgarciacampos@gmail.com>
2023-10-08 23:10:26 -0300

    Updating report

--- tp2/doc/code/workout.py
+++ tp2/doc/code/workout.py
@@ -1,12 +1,5 @@
 import input
 
-def binary_search(arr, left, right):
-    med = (left + right) // 2
-    if arr[med] != 0 and arr[med+1] == 0: return arr[med]
-    if arr[med] == 0:
-        return binary_search(arr, left, med)
-    return binary_search(arr, med, right)
-
 def getWorkOut(earn, energy):
     matrix = []
 
@@ -15,14 +8,15 @@ def getWorkOut(earn, energy):
         for i in range(idx+1):
             gain = min(e, energy[i])
             if i == 0:
-                prevTwoDays = binary_search(matrix[idx-2], 0, len(matrix[idx-2])) + gain if idx > 1 else 0
+                prevTwoDays = matrix[idx-2][idx-2] + gain if idx > 1 else 0
                 arr[i] = max(prevTwoDays, gain)
                 continue
             contWorkOut = matrix[idx-1][i-1] + gain if idx > 0 else 0
-            lastWorkOut = arr[i-1] if i > 0 else 0
+            lastWorkOut = arr[i-1]
             arr[i] = max(contWorkOut, lastWorkOut)
         matrix.append(arr)
 
+    print(matrix[len(earn)-1][len(energy)-1])
     return matrix
 
 def main():
